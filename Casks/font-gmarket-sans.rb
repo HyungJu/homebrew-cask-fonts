603e722453b6bd48177a653653c3748602affd58
HyungJu <sungkisa@naver.com>
2022-05-07 14:57:43 +0900

    Add gmarket-sans

---
+++ Casks/font-gmarket-sans.rb
@@ -0,0 +1,12 @@
+cask "font-gmarket-sans" do
+  version :latest
+  sha256 :no_check
+
+  url "http://script.ebay.co.kr/fonts/GmarketSansOTF.zip"
+  name "Gmarket Sans"
+  homepage "http://company.gmarket.co.kr/company/about/company/company--font.asp"
+
+  font "GmarketSansBold.otf"
+  font "GmarketSansLight.otf"
+  font "GmarketSansMedium.otf"
+end
